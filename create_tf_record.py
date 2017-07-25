4c09fd8499819d067a59e1d32d8e0227eb9e5c99
Mircea Eliade <roszcz@gmail.com>
2017-07-25 19:06:17 +0200

    Minor cleanup

--- create_tf_record.py
+++ create_tf_record.py
@@ -42,7 +42,7 @@ from utils import dataset_util
 flags = tf.app.flags
 flags.DEFINE_string('data_dir', '', 'Root directory to raw pet dataset.')
 flags.DEFINE_string('output_dir', '', 'Path to directory to output TFRecords.')
-flags.DEFINE_string('label_map_path', 'data/pet_label_map.pbtxt',
+flags.DEFINE_string('label_map_path', 'label_map.txt',
                     'Path to label map proto')
 FLAGS = flags.FLAGS
 
